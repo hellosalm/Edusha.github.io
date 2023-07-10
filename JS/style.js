1cf04341b1e6822ffd669930a6d651597139d2c3
hellosalm <infohappydreamzone@gmail.com>
2023-07-11 04:11:10 +0530

    commit e

---
+++ JS/style.js
@@ -0,0 +1,13 @@
+
+
+function myfunction(){
+    let x=document.getElementById("menu");
+    if(x.style.display=="block"){
+        x.style.display="none";
+    }else{
+        x.style.display="block"
+    }
+
+}
+
+
